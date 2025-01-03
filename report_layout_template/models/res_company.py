1dc4bb61385a610ebe2f2bc8440533b87c9d51a1
chhunlay <chhunlayking@gmail.com>
2025-01-03 12:12:27 +0700

    feat: add stree to base.document.layout

--- report_layout_template/models/res_company.py
+++ report_layout_template/models/res_company.py
@@ -23,3 +23,4 @@ class BaseDocumentLayout(models.TransientModel):
 
     khmer_name = fields.Char(related='company_id.khmer_name', readonly=True)
     khmer_address = fields.Char(related='company_id.khmer_address', readonly=True)
+    street = fields.Char(related='company_id.street', readonly=True)
