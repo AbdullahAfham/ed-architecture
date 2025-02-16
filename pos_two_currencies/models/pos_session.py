a56d1b73d0f4da99eebb243ff75e452d9df04f69
kimteng <kimteng.ley@peppermint.company>
2025-02-16 13:07:39 +0700

    chore: fix unexpected error on client side

--- pos_two_currencies/models/pos_session.py
+++ pos_two_currencies/models/pos_session.py
@@ -476,33 +476,6 @@ class PosSessionInherit(models.Model):
                     link=self._get_html_link()
                 ))
 
-    def _create_account_move(self, balancing_account=False, amount_to_balance=0, bank_payment_method_diffs=None):
-        """ Create account.move and account.move.line records for this session.
-
-        Side-effects include:
-            - setting self.move_id to the created account.move record
-            - reconciling cash receivable lines, invoice receivable lines and stock output lines
-        """
-        account_move = self.env['account.move'].create({
-            'journal_id': self.config_id.journal_id.id,
-            'date': fields.Date.context_today(self),
-            'ref': self.name,
-        })
-        self.write({'move_id': account_move.id})
-
-        data = {'bank_payment_method_diffs': bank_payment_method_diffs or {}}
-        data = self._accumulate_amounts(data)
-        data = self._create_non_reconciliable_move_lines(data)
-        data = self._create_bank_payment_moves(data)
-        data = self._create_pay_later_receivable_lines(data)
-        data = self._create_cash_statement_lines_and_cash_move_lines(data)
-        data = self._create_invoice_receivable_lines(data)
-        data = self._create_stock_output_lines(data)
-        if balancing_account and amount_to_balance:
-            data = self._create_balancing_line(data, balancing_account, amount_to_balance)
-
-        return data
-
     def _revalidate_session(self, balancing_account=False, amount_to_balance=0, bank_payment_method_diffs=None):
         for record in self:
             cash_difference_before_statements = record.cash_register_difference
