7da74899573713c6eee025f561e063e80a1e36ee
ADMINNUN <sophanonnun07@gmail.com>
2025-04-08 11:14:16 +0700

    update branch

--- report_layout_template/__manifest__.py
+++ report_layout_template/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': "Report Layout Templates",
     'description': "",
 
-    ############################ERP#####################################
+    ############################ERP######################################
     # Author
     'author': 'ERP CAMBODIA',
     'website': 'https://www.erpcambodia.biz/',
@@ -17,7 +17,7 @@
 
     # Technical
     'support': 'info@erpcambodia.biz',
-    ############################ERP#####################################
+    ############################ERP######################################
     'depends': [
         'web',
         'account'
