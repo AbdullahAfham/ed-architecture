b091c5c8d914c0711b6883624ad4cc8f4c8d172e
Abdullah Afham <afhamrazik@gmail.com>
2025-11-26 10:34:47 +0700

    Fix model name for PurchaseOrder alias

--- purchase_order_email_alias/models/purchase_order.py
+++ purchase_order_email_alias/models/purchase_order.py
@@ -1,9 +1,10 @@
 from odoo import models
 
 class PurchaseOrder(models.Model):
+    # We EXTEND existing purchase.order model and ADD mail.alias.mixin
     _inherit = ['purchase.order', 'mail.alias.mixin']
 
     def _alias_get_creation_values(self):
-        # When PO is created from email, you can set default values here
+        # Optional: adjust default values when PO created from email
         values = super()._alias_get_creation_values()
         return values
