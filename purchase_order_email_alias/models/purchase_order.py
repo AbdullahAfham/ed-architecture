7e87dc7bc905d08f281fbdbd77a5b7f0f9ecd148
Abdullah Afham <afhamrazik@gmail.com>
2025-11-26 10:56:15 +0700

    Add full mail.alias.mixin support for PO

--- purchase_order_email_alias/models/purchase_order.py
+++ purchase_order_email_alias/models/purchase_order.py
@@ -1,10 +1,24 @@
 from odoo import models
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = ['purchase.order', 'mail.alias.mixin']
 
-    # Add alias support safely
-    _mail_alias = True
+    # REQUIRED: this method tells Odoo which field stores the alias.
+    def _get_alias_model_name(self, vals):
+        return 'purchase.order'
 
+    # REQUIRED: store alias record
+    def _get_alias_domain(self):
+        return None
+
+    # REQUIRED: how to create PO from email
+    @classmethod
+    def _alias_get_fields(cls):
+        return ['partner_id', 'date_order', 'origin', 'user_id']
+
+    # REQUIRED: default values when PO created from email
     def _alias_get_creation_values(self):
-        return super(PurchaseOrder, self)._alias_get_creation_values()
+        return {
+            'partner_id': False,
+            'user_id': self.env.uid,
+        }
