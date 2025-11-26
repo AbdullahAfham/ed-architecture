4747939b692de5a012d7a1b8a8d8c039c1d158ab
Abdullah Afham <afhamrazik@gmail.com>
2025-11-26 10:48:32 +0700

    Enable alias support for Purchase Orders

--- purchase_order_email_alias/models/purchase_order.py
+++ purchase_order_email_alias/models/purchase_order.py
@@ -3,5 +3,8 @@ from odoo import models
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
+    # Add alias support safely
+    _mail_alias = True
+
     def _alias_get_creation_values(self):
         return super(PurchaseOrder, self)._alias_get_creation_values()
