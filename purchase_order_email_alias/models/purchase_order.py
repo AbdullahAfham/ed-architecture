9debc7899acce4d50168773abaed5595edb6cf8b
Abdullah Afham <afhamrazik@gmail.com>
2025-11-26 11:11:42 +0700

    Enable alias support for purchase.order (final safe version)

--- purchase_order_email_alias/models/purchase_order.py
+++ purchase_order_email_alias/models/purchase_order.py
@@ -3,4 +3,12 @@ from odoo import models
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
-    # clean file, no alias yet
+
+    # Enable e-mail alias support
+    def _get_alias_values(self):
+        """Return values used for the mail alias linked to purchase.order."""
+        return {
+            'alias_name': 'purchaseteam',      # alias name
+            'alias_model_id': self.env.ref('purchase.model_purchase_order').id,
+            'alias_contact': 'everyone',       # anyone can send PO email
+        }
